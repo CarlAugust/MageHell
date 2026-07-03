1478c9c69bd87437c20a4d391a1d9445f4d01a51
DumbAugust <carlaugust.sponberg@gmail.com>
2026-07-04 00:28:23 +0200

    Goodnight

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include <raylib.h>
 
 typedef int32_t i32;
+struct Player {
+    Vector2 position = { 0.0f, 0.0f };
+    Vector2 size = { 20.0f, 20.0f };
+    Vector2 hitbox = { 10.0f, 10.0f }; // Just example
+    i32 hp = 0;
+    float speed = 250;
+};
 
 int main(void) {
     const i32 screenWidth = 800;
@@ -11,11 +18,44 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "MageHell dogde the bullets pew pew");
     SetTargetFPS(165);
 
+    Player player;
+    Camera2D camera = {};
+    camera.offset = {(screenWidth - player.size.x) / 2, (screenHeight - player.size.y) / 2};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
     while (!WindowShouldClose()) {
+
+        float dt = GetFrameTime();
+
+        if (IsKeyDown(KEY_LEFT)) {
+            player.position.x -= player.speed * dt;
+        }
+        if (IsKeyDown(KEY_RIGHT)) {
+            player.position.x += player.speed * dt;
+        }
+        if (IsKeyDown(KEY_UP)) {
+            player.position.y -= player.speed * dt;
+        }
+        if (IsKeyDown(KEY_DOWN)) {
+            player.position.y += player.speed * dt;
+        }
+
+        camera.target = player.position;
         BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Welcome fool", screenWidth / 2, screenHeight / 2, 20, RED);
+
+        ClearBackground(WHITE);
+
+        i32 fontSize = 20;
+        const char* text = "Welcome fool";
+        i32 textLength = MeasureText(text, fontSize);
         
+        BeginMode2D(camera);
+
+        DrawRectangle(0.0f, 0.0f, 20, 20, BLUE);
+        DrawRectangleV(player.position, player.size, GREEN);
+
+        EndMode2D();
         EndDrawing();
     }
 
