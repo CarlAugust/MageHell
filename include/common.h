98b5e03343a10fa3e748069143f30b9f73ce3188
DumbAugust <carlaugust.sponberg@gmail.com>
2026-07-21 18:26:33 +0200

    Holy shit c++ is giving me an actual lobotomy

--- include/common.h
+++ include/common.h
@@ -3,49 +3,11 @@
 #include <raylib.h>
 #include <iostream>
 
+typedef uint8_t u8;
 typedef int32_t i32;
 typedef uint32_t u32;
 typedef int64_t i64;
 typedef uint64_t u64; 
-
-const Vector2 BasePlayerSize = { 8.0f, 16.0f };
-const float TileSize = 32.0f;
-
-struct Player {
-    Texture2D texture;
-    Vector2 position = { 0.0f, 0.0f };
-    Vector2 prevPosition = position;
-    Vector2 size = BasePlayerSize;
-    Vector2 hitboxPosition = {size.x / 2, size.y / 2};
-    i32 hp = 5;
-    float hitboxRadius = 2.0f;
-    float speed = 75;
-
-    // For handling health
-    Timer healthTimer = {};
-    bool isDamaged = false;
-
-    void takeDamage() {
-        if (isDamaged) return;
-
-        std::cout << "I took damage waiting 3 seconds\n";
-        hp--;
-        isDamaged = true;
-    }
-
-    void updateDamageState() {
-        if (!isDamaged) return;
-
-        if (healthTimer.elapses() > 0) {
-            isDamaged = false;
-            healthTimer.reset();
-        } 
-    }
-
-    Player() {
-        healthTimer.set(3.0f);
-    }
-};
 
 struct Timer {
     float time = 0.0f;
@@ -73,6 +35,45 @@ struct Timer {
         u64 elapse_count = static_cast<u64>(time / target);
         time = std::fmod(time, target);
         return elapse_count;
+    }
+};
+
+const Vector2 BasePlayerSize = { 8.0f, 16.0f };
+const float TileSize = 32.0f;
+
+struct Player {
+    Texture2D texture;
+    Vector2 position = { 0.0f, 0.0f };
+    Vector2 prevPosition = position;
+    Vector2 size = BasePlayerSize;
+    Vector2 hitboxPosition = {size.x / 2, size.y / 2};
+    i32 hp = 5;
+    float hitboxRadius = 2.0f;
+    float speed = 75;
+
+    // For handling health
+    Timer healthTimer = {0};
+    bool isDamaged = false;
+
+    void takeDamage() {
+        if (isDamaged) return;
+
+        std::cout << "I took damage waiting 3 seconds\n";
+        hp--;
+        isDamaged = true;
+    }
+
+    void updateDamageState() {
+        if (!isDamaged) return;
+
+        if (healthTimer.elapses() > 0) {
+            isDamaged = false;
+            healthTimer.reset();
+        } 
+    }
+
+    Player() {
+        healthTimer.set(3.0f);
     }
 };
 
