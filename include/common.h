f326dc2c6440323ed75802e073d363e0e4991bcf
DumbAugust <carlaugust.sponberg@gmail.com>
2026-07-21 16:34:27 +0200

    Forgot this file for last comit

--- include/common.h
+++ include/common.h
@@ -6,6 +6,7 @@
 
 #include <stdint.h>
 #include <raylib.h>
+#include <player.h>
 
 #include <iostream>
 #include <cmath>
@@ -45,42 +46,6 @@ struct Timer {
 
 const Vector2 BasePlayerSize = { 8.0f, 16.0f };
 const float TileSize = 32.0f;
-
-struct Player {
-    Texture2D texture;
-    Vector2 position = { 0.0f, 0.0f };
-    Vector2 prevPosition = position;
-    Vector2 size = BasePlayerSize;
-    Vector2 hitboxPosition = {size.x / 2, size.y / 2};
-    i32 hp = 5;
-    float hitboxRadius = 2.0f;
-    float speed = 75;
-
-    // For handling health
-    Timer healthTimer = {};
-    bool isDamaged = false;
-
-    void takeDamage() {
-        if (isDamaged) return;
-
-        std::cout << "I took damage waiting 3 seconds\n";
-        hp--;
-        isDamaged = true;
-    }
-
-    void updateDamageState() {
-        if (!isDamaged) return;
-
-        if (healthTimer.elapses() > 0) {
-            isDamaged = false;
-            healthTimer.reset();
-        } 
-    }
-
-    Player() {
-        healthTimer.set(3.0f);
-    }
-};
 
 /*
     Game data that gets shared across files.
